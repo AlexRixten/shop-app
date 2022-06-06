08a2c9bfaadc31a9be69ead5452075f52c736359
Neo <91592026+AlexRixten@users.noreply.github.com>
2022-06-06 22:07:09 +0300

    cart with orders

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
 
-export default function Header() {
+
+const showOrders = (props) => {
+  let sum = 0;
+  props.orders.forEach(el => sum += Number.parseFloat(el.price))
+  return (
+    <>
+      {props.orders.map(item => (
+        <Order key={item.id} item={item} onDelete={props.onDelete} />
+      ))}
+      <p className='sum'>Сумма: {new Intl.NumberFormat().format(sum)}$</p>
+    </>
+  )
+}
+
+const showNothing = () => {
+  return(
+    <div className='empty'>
+      <h2>Товаров нет</h2>
+    </div>
+  )
+}
+
+export default function Header(props) {
 
   const [cartOpen, setCartOpen] = useState(false)
-
-
 
   return (
     <header>
@@ -23,7 +44,8 @@ export default function Header() {
 
         {cartOpen && (
           <div className='shop-cart'>
-            123
+            {props.orders.length > 0 ?
+              showOrders(props) : showNothing()}
           </div>
         )}
       </div>
