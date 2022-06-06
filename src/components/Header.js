00e7f3aa31d8c72248dbb896b4ef462cda67ffbd
Neo <91592026+AlexRixten@users.noreply.github.com>
2022-06-06 18:06:49 +0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Header() {
+
+  const [cartOpen, setCartOpen] = useState(false)
+
+
+
   return (
     <header>
-        <div>
-            <span className='logo'>House Staff</span>
-            <ul className='nav'>
-                <li>Про нас</li>
-                <li>Контакты</li>
-                <li>Кабинет</li>
-            </ul>
-        </div>
-        <div className='presentation'></div>
+      <div>
+        <span className='logo'>House Staff</span>
+        <ul className='nav'>
+          <li>Про нас</li>
+          <li>Контакты</li>
+          <li>Кабинет</li>
+        </ul>
+        <FaShoppingCart
+          onClick={() => setCartOpen(!cartOpen)}
+          className={`shop-cart-button ${cartOpen && 'active'}`}
+        />
+
+        {cartOpen && (
+          <div className='shop-cart'>
+            123
+          </div>
+        )}
+      </div>
+      <div className='presentation'></div>
     </header>
   )
 }
